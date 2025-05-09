10a9007f798a4ec71ee492255502ed7c0a5a44d1
Martensven <martengullstrom@gmail.com>
2025-05-09 13:38:48 +0200

    initial commit with basic file structure

---
+++ frontend/src/sanityClient.js
@@ -0,0 +1,8 @@
+import { createClient } from '@sanity/client'
+
+export const client = createClient({
+    projectId: 'ugclfvqj',
+    dataset: 'production',
+    apiVersion: '2023-01-01',
+    useCdn: true,
+})
