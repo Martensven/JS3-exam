c9bd3f21842f319b0842a3dcbc5af36e3ac71157
Martensven <martengullstrom@gmail.com>
2025-05-16 10:09:35 +0200

    added recipeschema, removed authorschema

--- BaaS-Sanity/schemaTypes/index.js
+++ BaaS-Sanity/schemaTypes/index.js
@@ -1,9 +1,8 @@
-import author from './author.js'
 import category from './category.js'
 import recipe from './recipes.js'
 // import review from './review.js'
 
 export const schemaTypes = [
-    author,
+    recipe,
     category
 ]
