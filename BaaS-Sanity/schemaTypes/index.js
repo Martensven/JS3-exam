f493013d2e205cdcbf8dc6cbb935965f6501e394
Martensven <martengullstrom@gmail.com>
2025-05-16 09:23:58 +0200

    added scemas

--- BaaS-Sanity/schemaTypes/index.js
+++ BaaS-Sanity/schemaTypes/index.js
@@ -1,1 +1,9 @@
-export const schemaTypes = []
+import author from './author.js'
+import category from './category.js'
+import recipe from './recipes.js'
+// import review from './review.js'
+
+export const schemaTypes = [
+    author,
+    category
+]
