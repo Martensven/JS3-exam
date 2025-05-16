249cde8662a094e1bed8bf310deb20054352e5d4
Martensven <martengullstrom@gmail.com>
2025-05-16 10:26:28 +0200

    added reviewschema

--- BaaS-Sanity/schemaTypes/index.js
+++ BaaS-Sanity/schemaTypes/index.js
@@ -1,8 +1,10 @@
 import category from './category.js'
 import recipe from './recipes.js'
+import review from './review.js'
 // import review from './review.js'
 
 export const schemaTypes = [
     recipe,
-    category
+    category,
+    review
 ]
