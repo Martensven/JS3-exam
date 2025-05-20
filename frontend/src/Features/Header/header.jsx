b86082a7eca282ab138c097bf8d32f4d88b6dc8a
Sara <sara_lunden1@hotmail.com>
2025-05-20 10:28:34 +0200

    new style on frontend

--- frontend/src/Features/Header/header.jsx
+++ frontend/src/Features/Header/header.jsx
@@ -1,15 +1,23 @@
 import './header.css'
+import { LOGO } from './LOGO'
+import { NavButtons } from './NavButtons'
 import { Link } from 'react-router'
+import "../../assets/Kockmössan.jpg"
 
 export const Header = () => {
 
     return (
         <>
-            <div className='headerLogo'>🍴<i>ReceptFrossa</i>🍴</div>
+            <div className='headerContainer'>
+                <LOGO />
+                <img src='src\assets\Kockmössan.jpg' alt='Kockmössan' className='kockmössan' />
+            </div>
+
             <nav className="headerNav">
-                <Link to='/JS3-exam/'><button>🏠</button></Link>
-                <Link to='/JS3-exam/recipes'><button>📋</button></Link>
-                <Link to='/JS3-exam/recipes/example'><button>🍔</button></Link>
+                <NavButtons />
+                {/* <Link to='/JS3-exam/'> <button>🏠</button></Link>
+                <Link to='/JS3-exam/recipes'> <button>📋</button></Link>
+                <Link to='/JS3-exam/recipes/example'><button>🍔</button></Link> */}
             </nav>
         </>
     )
