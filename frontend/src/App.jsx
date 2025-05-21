65fff906a337f0293afb6af42af1e470b3769900
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-05-21 09:29:07 +0200

    categories v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import { Header } from './Features/Header/header.jsx'
-import { Start } from './Features/Start/start.jsx';
-import { RecipeCard } from './Features/Recipes/recipe-card/recipeCard.jsx';
-import { Recipes } from './Features/Recipes/recipe/recipe.jsx';
-import { Categories } from './Features/Categories/categories.jsx';
-
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./Features/Header/header.jsx";
+import { Start } from "./Features/Start/start.jsx";
+import { RecipeCard } from "./Features/Recipes/recipe-card/recipeCard.jsx";
+import { Recipes } from "./Features/Recipes/recipe/recipe.jsx";
+import { Categories } from "./Features/Categories/categories.jsx";
 
 function App() {
-
   return (
     <>
       <Header></Header>
 
-      <Routes basename='/JS3-exam'>
-        <Route path='/JS3-exam/' element={<Start />}></Route>
-        <Route path='/JS3-exam/recipes' element={<RecipeCard />}></Route>
-        <Route path='/JS3-exam/recipes/example' element={<Recipes />}></Route>
-        <Route path='/JS3-exam/categories' element={<Categories />}></Route>
-        
-
+      <Routes>
+        <Route path="/" element={<Start />}></Route>
+        <Route path="/recipes" element={<RecipeCard />}></Route>
+        <Route path="/recipes/example" element={<Recipes />}></Route>
+        <Route path="/categories" element={<Categories />}></Route>
+        <Route
+          path="/categories/category/:category"
+          element={<CategoryPage />}
+        ></Route>
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename='/JS3-exam' >
     <StrictMode>
       <App />
     </StrictMode>
