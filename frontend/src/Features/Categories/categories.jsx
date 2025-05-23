57543990ae6d34a35eec50cd6e50d6db7ebede97
Jesper Eriksson Stenberg <jespermail@gmail.com>
2025-05-23 08:40:47 +0200

    merge

--- frontend/src/Features/Categories/categories.jsx
+++ frontend/src/Features/Categories/categories.jsx
@@ -53,7 +53,7 @@ export const Categories = () => {
 
     <main className="categoriesContainer">
       <section className="categoriesButton">
-      <Link><button>Skapa recept</button></Link>
+      <Link to='/JS3-exam/recipes/create'><button>Skapa recept</button></Link>
       </section>
       <section className="grid-container">
         
