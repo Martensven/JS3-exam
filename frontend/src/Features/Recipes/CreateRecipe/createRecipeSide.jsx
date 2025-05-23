2caa57f927160df9cfc563aa3d2bbd3a9810e047
Sara <sara_lunden1@hotmail.com>
2025-05-23 09:45:04 +0200

    new content in createRecipeSIde

--- frontend/src/Features/Recipes/CreateRecipe/createRecipeSide.jsx
+++ frontend/src/Features/Recipes/CreateRecipe/createRecipeSide.jsx
@@ -1,9 +1,31 @@
 import "./createRecipeSide.css";
+import { useState } from "react";
 
 export const CreateRecipeSide = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const [recipe, setRecipe] = useState(null);
+    const [allCategories, setAllCategories] = useState([]);
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+    const [timeToCook, setTimeToCook] = useState(0);
+    const [portions, setPortions] = useState(0);
+    const [ingredients, setIngredients] = useState([]);
+    const [instructions, setInstructions] = useState([]);
+    const [selectedCategoryTitles, setSelectedCategoryTitles] = useState([]);
+    const [imageUrl, setImageUrl] = useState("");
+    const [newImageFile, setNewImageFile] = useState(null);
+    const [newIngredient, setNewIngredient] = useState("");
+    const [newInstruction, setNewInstruction] = useState("");
 
     return (
         <>
+        <main>
+            <section>
+                <h1>Här kan du skapa och lägga upp ett eget recept!</h1>
+                <p>Följ bara anvisningarna nedan.</p>
+            </section>
+        </main>
            
         </>
     )
