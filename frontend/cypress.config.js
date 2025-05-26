3ef2315d3903b0a2f9c0fd2e9903fe2efc1b6e16
Martensven <martengullstrom@gmail.com>
2025-05-26 14:14:58 +0200

    added cypress testing for editing recipe E2E

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,6 +1,10 @@
 import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'cypress'
 import vitePreprocessor from 'cypress-vite'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   e2e: {
@@ -8,7 +12,7 @@ export default defineConfig({
       on(
         'file:preprocessor',
         vitePreprocessor({
-          configFile: path.resolve(__dirname, './vite.config.ts'),
+          configFile: path.resolve(__dirname, 'vite.config.js'), // JS istället för TS
           mode: 'development',
         }),
       )
