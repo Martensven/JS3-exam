22a64db45f78a778f26c11a9fe7548d920b1883d
Alex Vong <avong4@gmail.com>
2025-05-27 13:05:47 +0200

    cypress test on sorting in collection

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,21 +1,14 @@
-import path from 'path'
-import { fileURLToPath } from 'url'
 import { defineConfig } from 'cypress'
 import vitePreprocessor from 'cypress-vite'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on) {
       on(
         'file:preprocessor',
-        vitePreprocessor({
-          configFile: path.resolve(__dirname, 'vite.config.js'), // JS istället för TS
-          mode: 'development',
-        }),
+        vitePreprocessor(),
       )
     },
   },
-});
+})
